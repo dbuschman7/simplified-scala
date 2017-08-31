e3191ada301beafa2486d31fc52ff00330b4c626
David Buschman <david.buschman@timeli.io>
2017-08-30 23:32:24 -0600

    added database connection handling and DLL helpers

---
+++ src/main/scala/me/lightspeed7/simplified/Database.scala
@@ -0,0 +1,76 @@
+package me.lightspeed7.simplified
+
+import java.sql.{Connection, DriverManager, ResultSet}
+
+object Database {
+
+  Class.forName("com.mysql.jdbc.Driver")
+
+  /**
+    * Connect to a MySQL database
+    *
+    * @param url - use jdbc:mysql://host:3306/database format here
+    * @param user
+    * @param password
+    * @return Connected object wrapping a connection
+    */
+  def connect(url: String, user: String, password: String): Connected = {
+    val database = url.substring(url.lastIndexOf("/") + 1)
+    val conn = DriverManager.getConnection(url, user, password)
+    new Connected(conn, database)
+  }
+
+  /**
+    * Use for testing ONLY !!!!
+    *
+    * @param rootPassword
+    * @param databaseName
+    */
+  def dropCreateLocalDatabase(rootPassword: String, databaseName: String): Unit = {
+    for (conn <- AutoCloseable(connect("jdbc:mysql://localhost:3306/mysql", "root", rootPassword))) {
+      conn.execute(s"DROP DATABASE IF EXISTS `$databaseName`")
+      conn.execute(s"CREATE DATABASE IF NOT EXISTS `$databaseName`")
+    }
+  }
+}
+
+
+class Connected private(connection: Connection, dbName: String, validTimeout: Int = 5000) extends java.lang.AutoCloseable {
+
+  import java.sql.ResultSet._
+
+  def database: String = this.dbName
+
+  def isValid: Boolean = connection.isValid(validTimeout)
+
+  def query[T](query: String, convert: ResultSet => T): List[T] = {
+    for (stmt <- AutoCloseable(connection.createStatement())) {
+      for (rs <- AutoCloseable(stmt.executeQuery(query))) {
+        new Iterator[T] {
+          def hasNext: Boolean = rs.next()
+
+          def next(): T = convert(rs)
+        }.toList
+      }
+    }
+  }
+
+  def stream(query: String, process: ResultSet => Unit): Unit = {
+    for (stmt <- AutoCloseable(connection.createStatement(TYPE_FORWARD_ONLY, CONCUR_READ_ONLY))) {
+      stmt.setFetchSize(Integer.MIN_VALUE)
+      for (rs <- AutoCloseable(stmt.executeQuery(query))) {
+        while (rs.next()) {
+          process(rs)
+        }
+      }
+    }
+  }
+
+  def execute(sql: String): Int = {
+    for (stmt <- AutoCloseable(connection.createStatement())) {
+      stmt.executeUpdate(sql)
+    }
+  }
+
+  override def close() = connection.close()
+}
---
+++ src/main/scala/me/lightspeed7/simplified/Table.scala
@@ -0,0 +1,58 @@
+package me.lightspeed7.simplified
+
+object Table {
+
+  object Partitioned {
+    def createPartitionStatement(partition: String): String = s"PARTITION p$partition VALUES LESS THAN ($partition)"
+
+    def makePartitionParts(column: String, partitionNames: Seq[String]): String = {
+      partitionNames
+        .map(createPartitionStatement)
+        .mkString( //
+          s"${System.lineSeparator()}/*!50100 PARTITION BY RANGE (`$column`) (${System.lineSeparator()}",
+          s",${System.lineSeparator()}",
+          ") */")
+    }
+
+    def createPartitionStatement(database: String, table: String, partition: String): String = {
+      s"""ALTER TABLE `$database`.`$table` ADD PARTITION (${createPartitionStatement(partition)})"""
+    }
+
+  }
+
+  def tableFooter: String = s"ENGINE=InnoDB DEFAULT CHARSET=utf8 ${System.lineSeparator()}"
+
+  case class Column(name: String, dataType: String, nullable: Boolean = false, autoIncrement: Boolean = false, default: Option[String] = None) {
+    def toLine: String = {
+      val nullPart = if (nullable) "" else " NOT NULL "
+      val autoPart = if (autoIncrement) " AUTO_INCREMENT " else ""
+      val defPart = default.map(i => s"DEFAULT $i").getOrElse("")
+
+      Seq(name, dataType, nullPart, autoPart, defPart).mkString(" ")
+    }
+  }
+
+  case class Key(name: String, columns: Seq[String]) {
+    def toLine: String = {
+      val cols = columns.toSeq.map { c => s"`${c}`" }
+      s"KEY `$name` (${cols.mkString(",")}) "
+    }
+  }
+
+  def column(name: String, dataType: String, nullable: Boolean = false, autoIncrement: Boolean = false, default: Option[String] = None): Column = {
+    Column(name, dataType, nullable, autoIncrement, default)
+  }
+
+  def key(name: String, columns: String*): Key = Key(name, columns.toSeq)
+
+  def makeTable(database: String, table: String, columns: Seq[Column], keys: Seq[Key]): String = {
+    s"""
+       |CREATE TABLE `$database`.`$table` (
+       |${columns.map(_.toLine).mkString(",${System.lineSeparator()}")}
+       |-- keys
+       |${keys.map(_.toLine).mkString(",${System.lineSeparator()}")}
+       |)
+       """.stripMargin
+  }
+
+}
